0c78918112c1bf3923ca9eb482617990c857f906
Muhammad Ali <nothingnoway00@gmail.com>
2026-08-10 07:27:35 -0400

    Added SQLite Persistence and removed empty chats

--- backend.py
+++ backend.py
@@ -1,23 +1,28 @@
-from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, BaseMessage
+from langchain_core.messages import BaseMessage
 from langchain_groq import ChatGroq
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 from dotenv import load_dotenv
 
+# load groq api key
 load_dotenv()
 
+# create groq chat model
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
     temperature=0.2
 )
 
+# create state for the graph
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages] 
 
 # ******************* Utitilies Functions *******************
 
+# create chat_mode to solve user querry
 def chat_mode(state: ChatState):
     messages = state['messages']
 
@@ -25,12 +30,24 @@ def chat_mode(state: ChatState):
 
     return {'messages': [response]}
 
+# create sqlite database and connect it to the backend
+conn = sqlite3.connect("chatbot.db", check_same_thread=False)
+checkpointer = SqliteSaver(conn)
 
-checkpointer = InMemorySaver()
+# to find total number of threads
+def retrieve_all_threads():
+    all_threads = set()
+
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
+
+# create and compile graph
 graph = StateGraph(ChatState)
 graph.add_node('chat_mode', chat_mode)
 graph.add_edge(START, 'chat_mode')
 graph.add_edge('chat_mode', END)
 chatbot = graph.compile(checkpointer=checkpointer)
-
 
--- frontend.py
+++ frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from backend import chatbot, CONFIG
+from backend import chatbot, retrieve_all_threads
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 import time
 import uuid
@@ -13,8 +13,8 @@ def gen_thread_id():
     return str(uuid.uuid4())
 
 # generate previous chat using thread id
-def gen_prechat():
-    state = chatbot.get_state(CONFIG)
+def gen_prechat(config):
+    state = chatbot.get_state(config)
     messages = state.values.get('messages', [])
 
     for message in messages:
@@ -28,32 +28,36 @@ def gen_prechat():
 
 # generate new chat with thread id
 def new_chat():
-    thread_id = gen_thread_id()
-    st.session_state.chat_threads.append(thread_id)
-    st.session_state.thread_id = thread_id
+    st.session_state.thread_id = None
 
 
 # initialize variables to track records of chats
 if "chat_threads" not in st.session_state:
-    st.session_state.chat_threads = []
+    st.session_state.chat_threads = retrieve_all_threads()
 
 if "thread_id" not in st.session_state:
-    st.session_state.thread_id = gen_thread_id()
-    st.session_state.chat_threads.append(
-        st.session_state.thread_id
-    )
+    st.session_state.thread_id = None
 
-# Get Current thread id for the chat
-CONFIG = {'configurable': {'thread_id': st.session_state.thread_id}}
 
-# generate previous chat of this thread id if available
-gen_prechat()        
+if st.session_state.thread_id is not None:
+    CONFIG = {'configurable': {'thread_id': st.session_state.thread_id}}
+    gen_prechat(CONFIG)
+
+       
 
 user_input = st.chat_input('Type here...')
 
 
 # if user enters then, display it
 if user_input:
+    if st.session_state.thread_id is None:
+        st.session_state.thread_id = gen_thread_id()
+
+    CONFIG = {'configurable': {'thread_id': st.session_state.thread_id}}
+
+    if st.session_state.thread_id not in st.session_state.chat_threads:
+        st.session_state.chat_threads.append(st.session_state.thread_id)
+
     with st.chat_message('user'):
         st.write(user_input)
 
@@ -83,7 +87,7 @@ if st.sidebar.button('New Chat'):
      new_chat()
      st.rerun()
 st.sidebar.header('My Conversations')
-for thread_id in st.session_state.chat_threads:
+for thread_id in st.session_state.chat_threads[::-1]:
      if st.sidebar.button(thread_id):
           st.session_state.thread_id = thread_id
           st.rerun()
